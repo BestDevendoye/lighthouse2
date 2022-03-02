fe94f13bc6ab48b3c7e675493ff5ae319c9a4b92
kaba-der <68657441+kaba-der@users.noreply.github.com>
2022-03-02 18:49:20 +0000

    v1 of app
    adding command line interface to support param: url to check

--- src/perf-check.js
+++ src/perf-check.js
@@ -1,32 +1,41 @@
-const fs = require('fs');
-const path = require('path');
+const readline = require('readline');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
+const { saveReport } = require('../lib/fileManager.js');
 
-// Base directory for saving reports
-const baseDir = path.join(__dirname, '/../reports');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Enter the url you want to check > ',
+});
 
 (async () => {
-  const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
-  const options = {
-    logLevel: 'info',
-    output: 'html',
-    onlyCategories: ['performance'],
-    port: chrome.port,
-  };
-  const runnerResult = await lighthouse('https://silo.der.sn', options);
-  // `.report` is the HTML report as a string
-  const reportHtml = runnerResult.report;
-  // Write report to new html file
-  fs.open(`${baseDir}/lhreport.html`, 'wx', async (err, fileDescriptor) => {
-    if (!err && fileDescriptor) {
-      await fs.writeFileSync(fileDescriptor, reportHtml);
-    } else {
-      console.log('Could not create new file, it may already exist!');
-    }
+  let checkUrl = '';
+  rl.prompt();
+  await rl.on('line', async (str) => {
+    checkUrl = str;
+    rl.close();
+    // perform the check
+    const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
+    const options = {
+      logLevel: 'info',
+      output: 'html',
+      onlyCategories: ['performance'],
+      port: chrome.port,
+    };
+    const runnerResult = await lighthouse(checkUrl, options);
+    const reportFolder = checkUrl.split('//')[1];
+    const fileName = new Date().toUTCString();
+    // `.report` is the HTML report as a string
+    const reportHtml = runnerResult.report;
+    // Write report to new html file
+    await saveReport(reportFolder, fileName, reportHtml, (err) => {
+      console.log(err);
+    });
+
+    // `.lhr` is the Lighthouse Result as a JS object
+    console.log('Report is done for', runnerResult.lhr.finalUrl);
+    console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+    await chrome.kill();
   });
-  // `.lhr` is the Lighthouse Result as a JS object
-  console.log('Report is done for', runnerResult.lhr.finalUrl);
-  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
-  await chrome.kill();
 })();
