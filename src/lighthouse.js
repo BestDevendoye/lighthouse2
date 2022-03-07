04cff4e8cb1f059cb211813919fff24a3dbaa6ee
Xavier Boubert <xboubert@kpmg.fr>
2022-03-07 14:26:55 +0100

    fix(lighthouse): make it works on Windows

--- src/lighthouse.js
+++ src/lighthouse.js
@@ -1,8 +1,15 @@
 import lighthouse from 'lighthouse';
 import chromeLauncher from 'chrome-launcher';
 import { saveReport } from '../lib/file-manager.js';
+import logger from '../lib/logger.js';
 
+const { info } = logger('Perf Check');
 const checkUrl = process.argv[process.argv.length - 1];
+const reportName = () => new Date()
+  .toISOString()
+  .replace(/:/g, '')
+  .replace(/\..*?$/, '')
+  .replace('T', '_');
 
 // perform the check
 (async () => {
@@ -14,16 +21,12 @@ const checkUrl = process.argv[process.argv.length - 1];
     port: chrome.port,
   });
 
-  const reportFolder = checkUrl.split('//')[1];
-  const fileName = new Date().toUTCString();
-  // `.report` is the HTML report as a string
-  const reportHtml = runnerResult.report;
   // Write report to new html file
-  saveReport(reportFolder, fileName, reportHtml);
+  saveReport(checkUrl.split('//')[1], reportName(), runnerResult.report);
 
   // `.lhr` is the Lighthouse Result as a JS object
-  console.log('Report is done for', runnerResult.lhr.finalUrl);
-  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+  info('Report is done for', runnerResult.lhr.finalUrl);
+  info('Performance score was', runnerResult.lhr.categories.performance.score * 100);
 
   await chrome.kill();
 })();
